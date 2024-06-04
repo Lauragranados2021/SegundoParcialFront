43cf2dc7f8b0e8228f6e9d3d211c140bff602269
LGRANA8 <158077445+LGRANA8@users.noreply.github.com>
2024-06-04 14:47:56 -0500

    2 commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = require("express").Router()
 const path = require("path")
 
 router.get("/", (req, res)=> res.sendFile(path.join(__dirname + "../../views/index.html")))
-router.get("/add", (req, res)=> res.sendFile(path.join(__dirname + "../../views/form.html")))
+router.get("/add", (req, res)=> res.sendFile(path.join(__dirname + "../../views/formt.html")))
 
 module.exports = router
